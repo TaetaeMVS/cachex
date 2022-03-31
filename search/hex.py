2d8eb4b4f13a55d390b6bfb2b66da47a047d91c6
Tae Yeong Kim <taeyeong2000@gmail.com>
2022-04-01 02:56:08 +1100

    add hex to repo

---
+++ search/hex.py
@@ -0,0 +1,57 @@
+# Class that represents a hexagonal grid board
+class Board:
+    def __init__(self, size, input=None):
+        self.size = size
+        self.input = input
+        self.hexes = {}
+        for i in range(size):
+            for j in range(size):
+                if (i, j) in input:
+                    self.hexes[i, j] = Hexagon(i, j, input[i, j])
+                else:
+                    self.hexes[i, j] = Hexagon(i, j, str(i) + ',' + str(j))
+        
+    
+    # Function to convert board from dict to list of coords for print_board helper function
+    def convert(self):
+        new_board = {}
+        n = self.size
+        for i in range(n):
+            for j in range(n):
+                print([self.hexes[i, j].get_coords()], self.hexes[i, j].value)
+                if self.hexes[i, j].value != [i, j]:
+                    new_board[i, j] = self.hexes[i, j].value
+        return new_board
+
+# Class that represents a hexagon
+class Hexagon:
+    def __init__(self, x, y, value='-'):
+        self.x = x
+        self.y = y
+        self.value = value
+        self.neighbours = []
+        self.visited = False
+        self.parent = None
+
+    # Function to test equality of two hexagons
+    def __eq__(self, Hexagon: object) -> bool:
+        return self.x == Hexagon.x and self.y == Hexagon.y
+
+    # Function to add (shift up and right) to hexagon
+    def __add__(self, Hexagon: object):
+        return Hexagon(self.x + Hexagon.x, self.y + Hexagon.y, self.value)
+    
+    # Function to subtract (shift down and left) from hexagon
+    def __sub__(self, Hexagon: object):
+        return Hexagon(self.x - Hexagon.x, self.y - Hexagon.y, self.value)
+    
+    # Function to test if hexagon is in bounds
+    def in_bounds(self, n: int) -> bool:
+        return 0 <= self.x < n and 0 <= self.y < n
+
+    # Function to check if hexagon is a neighbour
+    def is_neighbour(self, Hexagon: object) -> bool:
+        return abs(self.x - Hexagon.x) + abs(self.y - Hexagon.y) == 1
+    
+    def get_coords(self):
+        return [self.x, self.y]
