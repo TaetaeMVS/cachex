c115bd6c56f484646c13504f63c68860dc928a86
Tae Yeong Kim <taeyeong2000@gmail.com>
2022-04-02 19:03:27 +1100

    working branch

--- search/hex.py
+++ search/hex.py
@@ -6,18 +6,20 @@ class Board:
         self.hexes = {}
         self.start = None
         self.goal = None
+        self.solution = None
         for i in range(size):
             for j in range(size):
                 if (i, j) in input:
                     self.hexes[i, j] = Hexagon(i, j, input[i, j])
+                    self.hexes[i, j].board = self
                     if input[i, j] == "Start":
                         self.start = self.hexes[i, j]
                     if input[i, j] == "Goal":
                         self.goal = self.hexes[i, j]
                 else:
                     self.hexes[i, j] = Hexagon(i, j, str(i) + ',' + str(j))
-        
-    
+                    self.hexes[i, j].board = self
+          
     # Function to convert board from dict to list of coords for print_board helper function
     def convert(self):
         new_board = {}
@@ -28,6 +30,24 @@ class Board:
                     new_board[i, j] = self.hexes[i, j].value
         return new_board
 
+    # Function to update board with heuristic values for non goal, start or blocked hexes
+    def heuristics(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.hexes[i, j].value == "Goal" or self.hexes[i, j].value == "Start" or self.hexes[i, j].value == "b":
+                    continue
+                else:
+                    self.hexes[i, j] = self.hexes[i, j].heuristic
+
+    # Function to update board with coordinate values for non goal, start or blocked hexes
+    def coords(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.hexes[i, j].value == "Goal" or self.hexes[i, j].value == "Start" or self.hexes[i, j].value == "b":
+                    continue
+                else:
+                    self.hexes[i, j] = self.hexes[i, j].coords
+
 # Class that represents a hexagon
 class Hexagon:
     def __init__(self, x, y, value='-'):
@@ -37,6 +57,7 @@ class Hexagon:
         self.neighbours = []
         self.visited = False
         self.parent = None
+        self.board = None
 
     # Function to test equality of two hexagons
     def __eq__(self, Hexagon: object) -> bool:
@@ -61,3 +82,11 @@ class Hexagon:
     # Function to return coordinates of hexagon as a list
     def get_coords(self):
         return [self.x, self.y]
+
+    # Function to find the distance between two hexagons
+    def distance(self, Hexagon: object) -> int:
+        return abs(self.x - Hexagon.x) + abs(self.y - Hexagon.y)
+
+    # Heuristic function to find the distance between self and the goal hexagon
+    def heuristic(self) -> int:
+        return self.distance(self.board.goal)
